47d64d57bc69d1903735e0660a1e62f064dd017f
isaac <noerase12@gmail.com>
2020-01-12 12:47:54 +0800

    updates

--- Frontend/larReactNext/.next/static/runtime/webpack.js
+++ Frontend/larReactNext/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "463a9d4cc2dd67ffbe08";
+/******/ 	var hotCurrentHash = "77e4d949e38107218b00";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
