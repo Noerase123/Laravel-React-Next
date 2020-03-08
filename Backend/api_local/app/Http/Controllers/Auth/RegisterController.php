58bfc0d1c5628b82d31875d4c894f34aa6d44726
isaac <noerase12@gmail.com>
2020-03-08 10:45:55 +0800

    commiting

--- Backend/api_local/app/Http/Controllers/Auth/RegisterController.php
+++ Backend/api_local/app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -66,6 +66,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'role_id' => 1,
             'password' => Hash::make($data['password']),
         ]);
     }
