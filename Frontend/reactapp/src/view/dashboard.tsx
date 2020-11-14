28c12ffa23cca3087c99dc408916443c7b98e90a
isaac <noerase12@gmail.com>
2020-11-14 15:55:13 +0800

    updates

--- Frontend/reactapp/src/view/dashboard.tsx
+++ Frontend/reactapp/src/view/dashboard.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Container from '@material-ui/core/Container';
 import NavDrawer from '../components/dashboard/NavDrawer'
 import HomePage from '../components/dashboard/HomePage'
+import {DEFAULT_TITLE} from '../defaults/restData'
 
 export default function Dashboard() {
     return (
         <div>
-            <NavDrawer title="PMS">
+            <NavDrawer title={DEFAULT_TITLE}>
                 <Container>
                     <HomePage/>
                 </Container>
